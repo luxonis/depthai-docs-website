75cbfd965f2c6501f9c5177eb31c59ce155656c6
Derek Haynes <derek.haynes@gmail.com>
2020-02-11 10:12:22 -0700

    Open external links in a new tab

--- js/application.js
+++ js/application.js
@@ -49,3 +49,33 @@ function shortenMethodNames(string) {
   // "depthai.init_device(cmd_file_path) → bool" => "depthai.init_device"
   return string.replace(/\([\w=,]*\)\s*→\s*\w+/,'')
 }
+
+// Open External Links in a new tab
+$(document).ready(function() {
+
+   $("a[href^=http]").each(function(){
+
+      var excludes = [
+         'docs.luxonis.com',
+         'localhost'
+      ];
+      for(i=0; i<excludes.length; i++) {
+         if(this.href.indexOf(excludes[i]) != -1) {
+            return true; // continue each() with next link
+         }
+      }
+
+      if(this.href.indexOf(location.hostname) == -1) {
+
+           // attach a do-nothing event handler to ensure we can 'trigger' a click on this link
+           $(this).click(function() { return true; });
+
+           $(this).attr({
+               target: "_blank",
+               title: "Opens in a new window"
+           });
+
+           $(this).click(); // trigger it
+      }
+   })
+});
