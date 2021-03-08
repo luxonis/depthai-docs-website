a91d76352910acf220c0f2093c98c78aa6c79a86
Łukasz Piłatowski <lukpila29@gmail.com>
2021-03-08 15:34:11 +0100

    remove version from conf.py

--- source/conf.py
+++ source/conf.py
@@ -21,10 +21,6 @@ project = 'DepthAI Docs'
 copyright = '2020, Luxonis'
 author = 'Luxonis'
 
-# The full version, including alpha/beta/rc tags
-release = '0.3.0.0'
-
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
