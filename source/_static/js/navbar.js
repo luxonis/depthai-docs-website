35e1b1da2b0608a9cf789fcc7ab96a735d3a3dd0
Erol444 <erol123444@gmail.com>
2022-05-06 20:09:56 +0200

    Updated navbar to match new webpage style and logo

--- source/_static/js/navbar.js
+++ source/_static/js/navbar.js
@@ -19,18 +19,16 @@ function addNavbar() {
     "    <div class=\"lux-navbar\">\n" +
     "      <ul>\n" +
     "        <li><a href=\"https://luxonis.com\">\n" +
-    "          <img src=\"https://discuss.luxonis.com/assets/logo-fuhmgk6r.png\" alt=\"Luxonis\">\n" +
+    "          <img src=\"https://www.luxonis.com/logo.svg\" width=\"172\" height=\"48\" alt=\"Luxonis\">\n" +
     "        </a></li>\n" +
     "        <li><a href=\"https://docs.luxonis.com/\" class=\"lux-navbar-active\">Documentation</a></li>\n" +
     "        <li><a href=\"https://discuss.luxonis.com/\">Discuss</a></li>\n" +
-    "        <li><a href=\"https://luxonis.com/careers\">Careers</a></li>\n" +
-    "        <li><a href=\"https://luxonis.com/team\">Team</a></li>\n" +
-    "        <li><a href=\"https://luxonis.com/contact\">Contact</a></li>\n" +
+    "        <li><a href=\"https://www.luxonis.com/about-us\">About Us</a></li>\n" +
     "        <li><a href=\"http://shop.luxonis.com\">Shop</a></li>\n" +
     "      </ul>\n" +
     "    </div>\n" +
     "  </div>\n" +
-    "  <div id=\"lux-doc-navbar\" class=\"lux-navbar-container\">\n" +
+    "  <div id=\"lux-doc-navbar\">\n" +
     "    <div class=\"lux-navbar\">\n" +
     "      <ul>\n" +
     "        <li><a href=\"/en/latest/\">Main</a></li>\n" +
